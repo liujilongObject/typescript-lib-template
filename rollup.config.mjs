e592300a54abde7f7eddf8ee589c355fd8a3193c
liujilong <liujilong@360.cn>
2025-12-04 11:57:47 +0800

    feat: use api-extractor to merge the built type declaration files

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,18 +5,15 @@ import commonjs from '@rollup/plugin-commonjs'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import terser from '@rollup/plugin-terser'
 import babel from '@rollup/plugin-babel'
-import { dts } from 'rollup-plugin-dts'
 
 import path from 'path'
 import { fileURLToPath } from 'url'
-import { exec } from 'child_process'
-import { promisify } from 'util'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 const extensions = ['.js', '.ts']
 
-const resolveAlias = () =>alias({
+const resolveAlias = () => alias({
   entries: [
     { find: '@', replacement: path.resolve(__dirname, 'src') }
   ]
@@ -34,6 +31,7 @@ export default defineConfig([
         file: 'dist/index.esm.js',
         format: 'esm',
         exports: 'named',
+        // sourcemap: 'inline',
       },
     ],
     external: [/@babel\/runtime/],
@@ -54,26 +52,4 @@ export default defineConfig([
       terser(),
     ],
   },
-  // 合并类型声明文件
-  {
-    input: './dist/types/index.d.ts', // 指向生成的类型声明文件入口
-    output: [{ file: 'dist/index.d.ts', format: 'es' }], // 合并后的输出文件
-    plugins: [
-      resolveAlias(),
-      dts(),
-      {
-        name: 'cleanup-types',
-        buildEnd: async () => {
-          // 生产环境清除 dist/types 目录
-          if (process.env.BUILD === 'production') {
-            try {
-              await promisify(exec)('rimraf dist/types')
-            } catch (error) {
-              console.error('Error deleting dist/types directory:', error)
-            }
-          }
-        },
-      },
-    ],
-  }
 ])
